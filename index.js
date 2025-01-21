0987906d8ada9bfc1fdea86c2739f1fdf319ac4d
ULADZIMIR <01myit@gmail.com>
2025-01-21 17:15:57 +0300

    MAP.TASK

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 'use strict'
-const countries = [ 'BELARUS', 'USA', 'GBR', 'BELARUS', 'UKRAINE', 'BELARUS' ]
-const setOfCountries = new Set(countries)
-console.log( countries )
-console.log( setOfCountries )
-console.log( setOfCountries.size )
-console.log( setOfCountries.entries() )
-console.log( setOfCountries.values() )
-console.log( setOfCountries.has( 'USA' ) )
-console.log( setOfCountries.add( 'AUSTRALIA' ) )
-console.log( setOfCountries.keys() )
-setOfCountries.forEach( ( value, key ) => console.log( `${ key }: ${ value }` ) )
-const arrayOfCountries = Array.from( setOfCountries )
-console.log( arrayOfCountries )
-console.log( `SetOfCountries is Array: ${Array.isArray( setOfCountries )}`, `Typeof: ${typeof setOfCountries}`, setOfCountries )
-setOfCountries.clear()
-console.log( setOfCountries )
-console.log( new Set( 'ABCDEFG' ) )
+console.log( 'MAPS' )
+
+const weatherMap = new Map()
+console.log( weatherMap )
+const some = weatherMap.set( 'London', 10 ).set( 'Mensk', 18 )
+//? method SET returns pairs
+//? method DELETE returns boolean
+//? CLEAR - undefined
+// console.log( some )
+const numArr = [ 1, 2, 3 ]
+weatherMap.set( numArr, 'ARRAY' )
+weatherMap.set( 'some', 'SOME' )
+console.log( weatherMap.get( numArr ) )
+weatherMap.keys().forEach( k =>
+{
+    console.log(typeof k)
+    if ( Array.isArray( k ) ) console.log( k[ 2 ] )
+} )
+weatherMap.forEach( ( value, index, map ) => console.log( index, value, Array.isArray( map ) ) )
+
+const fastCreatingMap = new Map(
+    [
+        ['London', 10], ['Mensk', 18]
+    ]
+)
+console.log( fastCreatingMap )
+
+const weatherObject = {
+    'London': 10,
+    'Mensk': 18,
+    'Paris': 21
+}
+
+const mapFromObject = new Map( Object.entries( weatherObject ) )
+console.log( mapFromObject )
+console.log( [ ...mapFromObject ] )
+console.log( [ ...mapFromObject.keys() ] )
+console.log( [ ...mapFromObject.values() ] )
+
+//? TASK
+const weatherObjectTask = {
+    'London': 10,
+    'Mensk': 18,
+    'Paris': 21
+}
+const mapTask = new Map( Object.entries( weatherObjectTask ) )
+
+const reverseMap = new Map()
+mapTask.forEach( ( value, key ) =>
+{
+    reverseMap.set( value, key )
+} )
+console.log( reverseMap )
+
+const reverseMap_2 = new Map( [ ...mapTask ].map( arr => arr.reverse() ) )
+console.log( reverseMap_2 )
+console.log( mapTask )
